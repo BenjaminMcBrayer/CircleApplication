c0d7ad74adcf670927342da390c8a0ad9087ca43
benjamin <benjamin.mcbrayer@gmail.com>
2018-05-06 19:55:46 -0400

    added name, date, comments

--- src/com/circle/gc/CircleApp.java
+++ src/com/circle/gc/CircleApp.java
@@ -1,3 +1,6 @@
+//Benjamin McBrayer, 5.6.2018
+//This Java console program calculates a circle's circumference and area.
+
 package com.circle.gc;
 import java.util.Scanner;
 
@@ -11,14 +14,19 @@ public class CircleApp {
 		System.out.println("Welcome to the Grand Circus Circle Tester!");
 		
 		do {
+			//Prompt the user to enter a radius.
 			radius = Validator.getDouble(scnr, "Please enter the radius: ", 0, Double.MAX_VALUE);
 			
+			// Create a Circle object.
 			Circle c = new Circle(radius);
 			
+			//Display circumference.
 			System.out.println(c.getFormattedCircumference());
 			
+			//Display area.
 			System.out.println(c.getFormattedArea());
 			
+			//Prompt user to continue.
 			playAgain = Validator.getString(scnr, "Continue? (y/n)");
 			
 		} while (playAgain.equalsIgnoreCase("y"));
